4d6f0ed45bdf3463a46db4b8c0a565cd78887491
Aaron Pius <piuschbz@gmail.com>
2023-09-20 13:31:43 +0000

    Hopefully

---
+++ opcodes1.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * nop - does nothing
+ * @head: head to the list
+ * @line_number: command line number
+ */
+
+void nop(stack_t **head, unsigned int line_number)
+{
+	(void)head;
+	(void)line_number;
+}
